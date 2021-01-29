4dec8b3adb6cb1b18da312f9eb1f3f42775331ab
Mckenna Todd <matodd3@asu.edu>
2021-01-28 21:05:11 -0700

    unit testing creation and add reuseable template

--- globalkitchn/boards/views.py
+++ globalkitchn/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Board
 
 # -----COMMENT SHORTCUT CTRL+K+C
@@ -10,5 +10,10 @@ def home(request):
 
 
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
+    board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'new_topic.html', {'board': board})
--- globalkitchn/globalkitchn/urls.py
+++ globalkitchn/globalkitchn/urls.py
@@ -24,5 +24,6 @@ from boards import views  # importing views module from board app
 urlpatterns = [
     path('', views.home, name='home'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
 ]
